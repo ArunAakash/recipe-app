331b2e6de21d5aff708b517d081afbc560da6fde
Arun Aakash <aakashoffl0701@gmail.comecho>
2023-12-29 11:52:01 +0530

    add close button in homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,16 @@ import React from 'react'
 import styles from './Header.module.scss'
 import SearchBar from '../SearchBar/SearchBar'
 
-function Header({title = 'My App', setSearchQuery}) {
+function Header({ title = 'My App', setSearchQuery }) {
   return (
     // <div className='header'>
-    <div className={styles.header }>
-        <div className={styles.title}>{title}</div>
-        <SearchBar setSearchQuery={setSearchQuery}/>
+    <div className={styles.header}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.rightSection}>
+        <SearchBar setSearchQuery={setSearchQuery} />
+        <button className={styles.close} 
+        onClick = {() => setSearchQuery('')}>X</button>
+      </div>
     </div>
   )
 }
